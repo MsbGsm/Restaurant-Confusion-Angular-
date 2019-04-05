b36eaddd71a92ba63a181ba5addaf6ad03d6017c
MsbGsm <msbgsm@gmail.com>
2019-04-05 16:56:19 +0100

    Rxjs Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,31 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { pipe } from '@angular/core/src/render3/pipe';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
-  getDishes(): Promise<Dish[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 seconds delay
-      setTimeout(() => resolve(DISHES), 2000);
-    });
+  getDishes(): Observable<Dish[]> {
+    return of(DISHES).pipe(delay(2000));
   }
 
-  getDish(id: string): Promise<Dish> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 seconds delay
-      setTimeout(() => resolve(DISHES.filter(dish => (dish.id === id))[0]), 2000);
-    });
+  getDish(id: string): Observable<Dish> {
+    return of(DISHES.filter(dish => (dish.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedDish(): Promise<Dish> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 seconds delay
-      setTimeout(() => resolve(DISHES.filter(dish => dish.featured)[0]), 2000);
-    });
+  getFeaturedDish(): Observable<Dish> {
+    return of(DISHES.filter(dish => dish.featured)[0]).pipe(delay(2000));
   }
 
   constructor() { }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,31 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 import { LEADERS } from '../shared/leaders';
 import { Leader } from '../shared/leader';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(LEADERS), 2000);
-    });
+  getLeaders(): Observable<Leader[]> {
+    return of(LEADERS).pipe(delay(2000));
   }
 
-  getLeader(id: string): Promise<Leader> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(LEADERS.filter(leader => (leader.id === id))[0]), 2000);
-    });
+  getLeader(id: string): Observable<Leader> {
+    return of(LEADERS.filter(leader => (leader.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(LEADERS.filter(leader => leader.featured)[0]), 2000);
-    });
+  getFeaturedLeader(): Observable<Leader> {
+    return of(LEADERS.filter(leader => leader.featured)[0]).pipe(delay(2000));
   }
 
   constructor() { }
